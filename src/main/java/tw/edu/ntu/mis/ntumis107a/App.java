09a95e976b785c76355f001f92d3a8ec64a657dc
GaryLee <jakarta99@gmail.com>
2018-10-05 13:19:19 +0800

    Run HelloWorld.

--- src/main/java/tw/edu/ntu/mis/ntumis107a/App.java
+++ src/main/java/tw/edu/ntu/mis/ntumis107a/App.java
@@ -4,10 +4,10 @@ package tw.edu.ntu.mis.ntumis107a;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
 }
