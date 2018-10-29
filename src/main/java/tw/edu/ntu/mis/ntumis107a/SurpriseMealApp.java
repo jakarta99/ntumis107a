f101d955224fbd9c297c488759d19b617a50607f
wenyilee <wenyilee@laurens-mbp>
2018-10-29 19:33:03 +0800

    hello

---
+++ src/main/java/tw/edu/ntu/mis/ntumis107a/SurpriseMealApp.java
@@ -0,0 +1,21 @@
+package tw.edu.ntu.mis.ntumis107a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class SurpriseMealApp {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurpriseMealApp.class, args);
+
+	}
+
+}
