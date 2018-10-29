aa7abe476c8457710177ed81d20aa2e68b347465
nalacheng <chengnala1396@gmail.com>
2018-10-29 20:45:14 +0800

    Food after two moves

--- src/main/java/tw/edu/ntu/mis/ntumis107a/entity/Food.java
+++ src/main/java/tw/edu/ntu/mis/ntumis107a/entity/Food.java
@@ -4,4 +4,28 @@ public class Food {
 	private Long id;
 	private String code;
 	private String name;
+	
+	@Override
+	public String toString() {
+		return "Food [id=" + id + ", code=" + code + ", name=" + name + "]";
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
