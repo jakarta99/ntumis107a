bb72bc082b58747a4df5ecb8fea19270fc22f335
laurenhaute <b06704033@ntu.edu.tw>
2018-11-05 21:14:38 +0800

    eei

--- src/main/java/tw/edu/ntu/mis/ntumis107a/entity/Drink.java
+++ src/main/java/tw/edu/ntu/mis/ntumis107a/entity/Drink.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Drink {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(columnDefinition="bigserial")
 	private Long id;
 	
