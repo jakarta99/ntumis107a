534bd4a076ac178bcb30372f58ec6d7453795619
laurenhaute <b06704033@ntu.edu.tw>
2018-11-05 20:32:43 +0800

    erantr

--- src/main/java/tw/edu/ntu/mis/ntumis107a/web/DrinkController.java
+++ src/main/java/tw/edu/ntu/mis/ntumis107a/web/DrinkController.java
@@ -1,14 +1,34 @@
 package tw.edu.ntu.mis.ntumis107a.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tw.edu.ntu.mis.ntumis107a.dao.BookDao;
+import tw.edu.ntu.mis.ntumis107a.dao.DrinkDao;
+import tw.edu.ntu.mis.ntumis107a.entity.Book;
+import tw.edu.ntu.mis.ntumis107a.entity.Drink;
+
 @RestController
 public class DrinkController {
+
+	@Autowired
+	private DrinkDao drinkDao;
+	
 	@RequestMapping("/drink")
 	public String listPage() {
-		return "Drink";
+		
+		List<Drink> drinks = drinkDao.findAll();
+		
+		String html = "";
+		
+		for(Drink drink:drinks) {
+			html += "name="+drink.getName()+"<br/>";
+		}
+		
+		
+		return html;
 	}
-	
-
 }
