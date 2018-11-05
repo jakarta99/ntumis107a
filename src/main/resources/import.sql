7f3c30291de240a1fd65f1fcf07609f05e899801
GaryLee <jakarta99@gmail.com>
2018-11-05 21:03:42 +0800

    insert/update/delete.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 INSERT INTO BOOK(code, name) VALUES('AAA-001', 'Happy to Learn Java Basic');
 INSERT INTO BOOK(code, name) VALUES('AAA-002', 'Happy to Learn Java Web');
-INSERT INTO BOOK(code, name) VALUES('AAA-003', 'Happy to Learn Java EE');
+INSERT INTO BOOK(code, name) VALUES('AAA-003', 'Bad to Learn Java EE');
 
 INSERT INTO MOVIE(code, name) VALUES('A1','Harry Potter');
 INSERT INTO MOVIE(code, name) VALUES('A2','Bananas are happy');
