3e1b2eedc9d831673003ee8d2f53c78622eb3c6f
laurenhaute <b06704033@ntu.edu.tw>
2018-11-05 18:24:12 +0800

    i

--- src/main/java/tw/edu/ntu/mis/ntumis107a/web/DrinkController.java
+++ src/main/java/tw/edu/ntu/mis/ntumis107a/web/DrinkController.java
@@ -1,5 +1,14 @@
 package tw.edu.ntu.mis.ntumis107a.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DrinkController {
+	@RequestMapping("/drink")
+	public String listPage() {
+		return "Drink";
+	}
+	
 
 }
