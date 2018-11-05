a06001a9d7ad47b0f905a1e2f8bfa341f4c3cd20
ShinyenChiu <shinyen224466@gmail.com>
2018-11-05 20:42:17 +0800

    movie

---
+++ src/main/java/tw/edu/ntu/mis/ntumis107a/dao/MovieDao.java
@@ -0,0 +1,9 @@
+package tw.edu.ntu.mis.ntumis107a.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tw.edu.ntu.mis.ntumis107a.entity.Movie;
+
+public interface MovieDao extends JpaRepository<Movie, Long> {
+
+}
