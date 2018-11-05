6a000886be4e5905b6a404cdc4256e7a3ea8fc74
GaryLee <jakarta99@gmail.com>
2018-11-05 19:09:26 +0800

    add spring-data-jpa in pom.xml

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO BOOK(code, name) VALUES('AAA-001', 'Happy to Learn Java Basic');
+INSERT INTO BOOK(code, name) VALUES('AAA-002', 'Happy to Learn Java Web');
+INSERT INTO BOOK(code, name) VALUES('AAA-003', 'Happy to Learn Java EE');
