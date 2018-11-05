2c2689dfe2015e1ffbefbadd824a787f2a8b19f2
laurenhaute <b06704033@ntu.edu.tw>
2018-11-05 20:23:41 +0800

    A

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,7 @@
 INSERT INTO BOOK(code, name) VALUES('AAA-001', 'Happy to Learn Java Basic');
 INSERT INTO BOOK(code, name) VALUES('AAA-002', 'Happy to Learn Java Web');
 INSERT INTO BOOK(code, name) VALUES('AAA-003', 'Happy to Learn Java EE');
+
+INSERT INTO DRINK(code, name) VALUES('AAA-004', 'coke');
+INSERT INTO DRINK(code, name) VALUES('AAA-005', 'sprite');
+INSERT INTO DRINK(code, name) VALUES('AAA-006', 'fanta');
