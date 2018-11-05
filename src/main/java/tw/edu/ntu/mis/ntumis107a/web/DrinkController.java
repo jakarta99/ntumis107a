ea92420509b3b4ae20c447ce4ecd54800fec0986
GaryLee <jakarta99@gmail.com>
2018-11-05 20:45:59 +0800

    remove unused import.

--- src/main/java/tw/edu/ntu/mis/ntumis107a/web/DrinkController.java
+++ src/main/java/tw/edu/ntu/mis/ntumis107a/web/DrinkController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import tw.edu.ntu.mis.ntumis107a.dao.BookDao;
 import tw.edu.ntu.mis.ntumis107a.dao.DrinkDao;
-import tw.edu.ntu.mis.ntumis107a.entity.Book;
 import tw.edu.ntu.mis.ntumis107a.entity.Drink;
 
 @RestController
