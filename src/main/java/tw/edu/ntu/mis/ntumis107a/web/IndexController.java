6a9c8394dce63d27d1f0987f654c5479a0eb679d
nalacheng <chengnala1396@gmail.com>
2018-11-26 19:57:42 +0800

    practice buttons

--- src/main/java/tw/edu/ntu/mis/ntumis107a/web/IndexController.java
+++ src/main/java/tw/edu/ntu/mis/ntumis107a/web/IndexController.java
@@ -35,4 +35,24 @@ public class IndexController {
 		return "/choose-meal";
 		
 	}
+	@RequestMapping("/test1")
+	public String test1() {
+		return "/test1";
+	}
+	@RequestMapping("/test2")
+	public String test2() {
+		return "/test2";
+	}
+	@RequestMapping("/test3")
+	public String test3() {
+		return "/test3";
+	}
+	@RequestMapping("/test4")
+	public String test4() {
+		return "/test4";
+	}
+	@RequestMapping("/test5")
+	public String test5() {
+		return "/test5";
+	}
 }
