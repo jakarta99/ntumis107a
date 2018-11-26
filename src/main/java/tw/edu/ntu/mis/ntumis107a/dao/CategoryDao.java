0bbc6dd7023b7107e9ebe613a0ec132f07b9db3e
nalacheng <chengnala1396@gmail.com>
2018-11-26 15:32:48 +0800

    fixed problem with category dao

--- src/main/java/tw/edu/ntu/mis/ntumis107a/dao/CategoryDao.java
+++ src/main/java/tw/edu/ntu/mis/ntumis107a/dao/CategoryDao.java
@@ -1,8 +1,10 @@
 package tw.edu.ntu.mis.ntumis107a.dao;
 
-import java.util.Locale.Category;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import tw.edu.ntu.mis.ntumis107a.entity.Category;
 
 
 public interface CategoryDao extends JpaRepository<Category,Long>{
