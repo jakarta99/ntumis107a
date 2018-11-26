3a3af0ae9035009f2b366bf563ba438621c3c277
SkySpiriT <b04602031.ntu.edu.tw>
2018-11-26 15:34:44 +0800

    update

--- src/main/java/tw/edu/ntu/mis/ntumis107a/web/IndexController.java
+++ src/main/java/tw/edu/ntu/mis/ntumis107a/web/IndexController.java
@@ -1,6 +1,7 @@
 package tw.edu.ntu.mis.ntumis107a.web;
 
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import tw.edu.ntu.mis.ntumis107a.dao.MealDao;
 import tw.edu.ntu.mis.ntumis107a.dao.RestaurantDao;
 import tw.edu.ntu.mis.ntumis107a.entity.Meal;
+import tw.edu.ntu.mis.ntumis107a.entity.Restaurant;
 
 @RestController
 public class IndexController {
@@ -23,22 +25,27 @@ public class IndexController {
 	@RequestMapping("/")
 	String home() {
 		
+		
 		Meal meal1 = new Meal();
 		meal1.setName("豬排飯");
 		meal1.setCategoryid("1");
 		meal1.setMeatcategoryid("1");
-		meal1.setRestaurantid("001");
+		meal1.setRestaurantid("1");
 		meal1.setPrice(100L);
-		meal1.setId();
-		
+
 		mealDao.save(meal1);
 		
-		Restaurant 
+		Restaurant restaurant1= new Restaurant();
+		restaurant1.setName("好吃餐廳");
+
+		
+		restaurantDao.save(restaurant1);
 		
 		
 		
-		
-		List<Meal>meals=mealDao.findByCategoryIs("1");
+		Random ran = new Random();
+
+		List<Meal>meals=mealDao.findByID(ran.nextInt(42));
 		
 		String html = "";
 		
