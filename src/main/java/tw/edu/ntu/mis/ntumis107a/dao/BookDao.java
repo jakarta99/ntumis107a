9e59ae2b7eb6362a95e0272667a5f9f1f9338bc0
SkySpiriT <b04602031.ntu.edu.tw>
2018-12-05 19:53:14 +0800

    fix merge problem

--- src/main/java/tw/edu/ntu/mis/ntumis107a/dao/BookDao.java
+++
@@ -1,9 +0,0 @@
-package tw.edu.ntu.mis.ntumis107a.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tw.edu.ntu.mis.ntumis107a.entity.Book;
-
-public interface BookDao extends JpaRepository<Book, Long> {
-
-}
--- src/main/java/tw/edu/ntu/mis/ntumis107a/dao/FoodDao.java
+++
@@ -1,9 +0,0 @@
-package tw.edu.ntu.mis.ntumis107a.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tw.edu.ntu.mis.ntumis107a.entity.Food;
-
-public interface FoodDao extends JpaRepository<Food, Long>{
-
-}
